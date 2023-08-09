d513088d663607a4c5763f6bee05fc8363f8115b
Hyeonsu Bang <1021gustn@gmail.com>
2023-08-10 03:06:13 +0900

    파일 업로드 3

---
+++ src/main/java/com/study/board/dto/asdfDTO.java
@@ -0,0 +1,52 @@
+package com.study.board.dto;
+
+import com.study.board.entity.BoardEntity;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor // 기본생성자
+@AllArgsConstructor // 모든 필드를 매개변수로 하는 생성자
+public class asdfDTO { // DTO(Data Transfer Object) 데이터 전송하는 객체 VO랑 Bean 이랑 비슷하다
+    private Long id;
+    private String boardWriter;
+    private String boardPass;
+    private String boardTitle;
+    private String boardContents;
+    private int boardHits; //조회수
+    private LocalDateTime boardCreatedTime; //작성시간
+    private LocalDateTime boardUpdatedTime; //수정시간
+
+    private MultipartFile boardFile; // 실제 파일을 담아줄 수 있는 역할 save.html -> Controller 파일 담는 용도
+    //두 가지를 사용하는 이유는 업로드하는 파일 이름에서 충돌나지 않게 하기 위해
+    private String originalFileName; // 파일 이름
+    private String storedFileName; // 서버 저장용 파일 이름
+    private int fileAttached; // 파일 첨부 여부(첨부 1, 미첨부 0)
+
+    public asdfDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
+        this.id = id;
+        this.boardWriter = boardWriter;
+        this.boardTitle = boardTitle;
+        this.boardHits = boardHits;
+        this.boardCreatedTime = boardCreatedTime;
+    }
+
+    public static BoardDTO toBoardDTO(BoardEntity boardEntity) {
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        boardDTO.setBoardCreatedTime(boardEntity.getCreatedTime());
+        boardDTO.setBoardUpdatedTime(boardEntity.getUpdatedTime());
+
+        return boardDTO;
+    }
+
+
+}
